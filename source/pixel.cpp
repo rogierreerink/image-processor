67afeb8d874d069c621362e67ba5071f10e0d8ce
Rogier Reerink <me@rogier.io>
2020-10-27 18:13:42 +0100

    Generating and using a lookup table is slightly faster than calculating the new brightness/contrast values and setting their limits on every pixel iteration.

--- source/pixel.cpp
+++ source/pixel.cpp
@@ -16,20 +16,23 @@ using namespace std;
 
 void Pixel::Brightness(Mat &image, int shift) {
 
+	uint8_t brightnessLUT[256];
+	for (int i = 0; i < 256; i++) {
+		int value = i + shift;
+		if (value > 255) {
+			brightnessLUT[i] = 255;
+		} else if (value < 0) {
+			brightnessLUT[i] = 0;
+		} else {
+			brightnessLUT[i] = value;
+		}
+	}
+
 	for (int channel = 0; channel < image.channels(); channel++) {
 		for (int row = 0; row < image.rows; row++) {
 			for (int col = 0; col < image.cols; col++) {
-
-				const int newPixel 
-					= (float)image.at<Vec3b>(row, col)[channel] + shift;
-
-				if (newPixel > 255) {
-					image.at<Vec3b>(row, col)[channel] = 255;
-				} else if (newPixel < 0) {
-					image.at<Vec3b>(row, col)[channel] = 0;
-				} else {
-					image.at<Vec3b>(row, col)[channel] = (uchar)newPixel;
-				}
+				image.at<Vec3b>(row, col)[channel] =
+					brightnessLUT[image.at<Vec3b>(row, col)[channel]];
 			}
 		}
 	}
@@ -37,20 +40,23 @@ void Pixel::Brightness(Mat &image, int shift) {
 
 void Pixel::Contrast(Mat &image, float factor) {
 
+	uint8_t contrastLUT[256];
+	for (int i = 0; i < 256; i++) {
+		float value = (i - 127.0) * factor + 127.0;
+		if (value > 255) {
+			contrastLUT[i] = 255;
+		} else if (value < 0) {
+			contrastLUT[i] = 0;
+		} else {
+			contrastLUT[i] = value;
+		}
+	}
+
 	for (int channel = 0; channel < image.channels(); channel++) {
 		for (int row = 0; row < image.rows; row++) {
 			for (int col = 0; col < image.cols; col++) {
-
-				const float newPixel 
-					= ((float)image.at<Vec3b>(row, col)[channel] - 127) * factor + 127;
-
-				if (newPixel > 255) {
-					image.at<Vec3b>(row, col)[channel] = 255;
-				} else if (newPixel < 0) {
-					image.at<Vec3b>(row, col)[channel] = 0;
-				} else {
-					image.at<Vec3b>(row, col)[channel] = (uchar)newPixel;
-				}
+				image.at<Vec3b>(row, col)[channel] =
+					contrastLUT[image.at<Vec3b>(row, col)[channel]];
 			}
 		}
 	}
